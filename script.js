faa0b03485bbc2868a7061949c4614d89f891778
Catalin Pit <catalinpit@gmail.com>
2020-07-31 16:04:24 +0100

    Fetching a Quote API

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('it works!');
+const quote = document.getElementsByClassName('quote');
+const author = document.getElementsByClassName('author');
+
+// Quote API
+const getQuote = async () => {
+    const api = 'http://quotes.rest/qod.json?category=inspire';
+
+    try {
+        const response = await fetch(api);
+        const data = await response.json();
+
+        // Replace the HTML
+        quote.innerHTML = data.contents.quotes[0].quote;
+        author.innerHTML = data.contents.quotes[0].author;
+    } catch (err) {
+        //getQuote();
+        console.warn(err);
+    }
+}
+
+getQuote();
