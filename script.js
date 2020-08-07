d2ba21368bf210c8bb40735d5085ea911a5431ab
Catalin Pit <catalinpit@gmail.com>
2020-08-07 11:28:25 +0300

    Added tweeting functionality and changed the api

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const quote = document.getElementById('quote');
 const author = document.getElementById('author');
-const twitterButton = document.getElementsByClassName('twitter');
+const twitterButton = document.getElementById('twitter-button');
 
 // Quote API
 const getQuote = async () => {
-    const api = 'http://quotes.rest/qod.json?category=inspire';
+    const api = 'https://type.fit/api/quotes';
 
     try {
         const response = await fetch(api);
         const data = await response.json();
-        const quoteContent = data.contents.quotes[0].quote;
-        let authorQuote = data.contents.quotes[0].author;
+        const quoteContent = data[Math.floor((Math.random() * 1643) + 1)].text;
+        let authorQuote = data[Math.floor((Math.random() * 1643) + 1)].author;
         
         if (authorQuote === '') {
             author.innerText = 'Unknown'
@@ -18,11 +18,25 @@ const getQuote = async () => {
             author.innerText = authorQuote;
         }
 
+        if (quoteContent.length > 120) {
+            quote.classList.add('long-quote');
+        } else {
+            quote.classList.remove('long-quote');
+        }
+
         quote.innerText = quoteContent;
     } catch (err) {
         console.warn(err);
         getQuote();
     }
+}
+
+function tweetQuote() {
+    const quote = quote.innerText;
+    const author = author.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} by ${author}`;
+
+    window.open(twitterUrl, '_blank');
 }
 
 getQuote();
